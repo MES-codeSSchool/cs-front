cf7b8e686a83f1261fdd5b9f943016971e2f6b3d
Gustavo Moreira <gustavomn93@gmail.com>
2017-10-19 17:33:06 -0200

    Changing questions links button to click in the intere box

--- src/Page/Questions/QuestionRoot.elm
+++ src/Page/Questions/QuestionRoot.elm
@@ -57,7 +57,7 @@ viewList m =
 
 questionInfo : QuestionInfo -> Html Msg
 questionInfo cls =
-    div [ class "question-info-card" ]
+    div [ class "question-info-card", onClick (ChangeRoute (QuestionList "base")) ] 
         [ Ui.Generic.icon [ class "question-info-card__icon" ] cls.icon
         , h1 [ class "question-info-card__title" ]
             [ text cls.questionName
@@ -65,7 +65,6 @@ questionInfo cls =
         , p [ class "question-info-card__description" ]
             [ text cls.shortDescription
             ]
-        , span [ onClick (ChangeRoute (QuestionList "base")), class "mobile-button__item" ] [ h1 [class "mobile-button__fonts"] [text "Acessar"] ]
         ]
 
 
